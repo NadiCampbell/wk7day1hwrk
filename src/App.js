6c6abc3068337c8587b4c51e11dcc87ea4b2e7a0
Nadi Campbell <nadicampbell97@hotmail.com>
2023-07-10 17:29:42 +0100

    finished_mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    const [tasks, setTasks] = useState([
+      {name : "Buy shopping", priority: "high"},
+      {name: "Clean bathroom", priority: "low"},
+      {name: "Car's MOT", priority: "high"}
+    ]);
 
-export default App;
+    const [newTask, setNewTask] = useState("");
+
+    const handleNewTask = (event) => {
+      setNewTask(event.target.value);
+    }
+
+
+    const handleAddToDo = (event) => {
+      event.preventDefault();
+      setTasks((previousState) => {
+        return [
+          ...previousState,
+          newTask
+        ]
+      });
+      setNewTask("");
+    } 
+    
+    const taskNodes = tasks.map(function(task, index){
+      return (
+        <li key={index} className={task.priority ? "high" : "low"}>
+          <span>
+            {task.name}
+          </span>
+        </li>
+      )
+    });
+
+    return (
+      <div className="App">
+        <h1>ToDo's</h1>
+        <form onSubmit={handleAddToDo}>
+        <label className="new-todo"></label>
+        <input id="new-todo" type="text" value={newTask} onChange={handleNewTask}/>
+        <label>High<input type="radio" value="high"/></label>
+        <label>Low<input type="radio" value="low"></input></label>
+        <button onClick={handleAddToDo}>Save Item</button>
+        </form>
+        <ul>
+            {taskNodes}
+        </ul>
+      </div>
+    );
+  };
+
+  export default App;
